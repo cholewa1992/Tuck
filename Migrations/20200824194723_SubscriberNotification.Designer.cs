8cc12ae32d31d3bc4563acbfb577902105e5117d
Carl Nettelblad <carl.nettelblad@gmail.com>
2020-08-24 21:56:46 +0200

    Added SubscriberAlert column to Subscription table

--- Migrations/20200824194723_SubscriberNotification.Designer.cs
+++ Migrations/20200824194723_SubscriberNotification.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tuck;
 
 namespace Tuck.Migrations
 {
     [DbContext(typeof(TuckContext))]
-    partial class TuckContextModelSnapshot : ModelSnapshot
+    [Migration("20200824194723_SubscriberNotification")]
+    partial class SubscriberNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
