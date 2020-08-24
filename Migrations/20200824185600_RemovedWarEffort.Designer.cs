12c47c59f8e9dfe560dc96676b631fe541f36ea0
Jacob Benjamin Cholewa <Jacob@cholewa.dk>
2020-08-24 20:59:18 +0200

    Removed the wareffort module (#22)

--- Migrations/20200824185600_RemovedWarEffort.Designer.cs
+++ Migrations/20200824185600_RemovedWarEffort.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tuck;
 
 namespace Tuck.Migrations
 {
     [DbContext(typeof(TuckContext))]
-    partial class TuckContextModelSnapshot : ModelSnapshot
+    [Migration("20200824185600_RemovedWarEffort")]
+    partial class RemovedWarEffort
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
