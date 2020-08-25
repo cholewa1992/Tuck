6e0bf3280e46a17d32f1c0631e3bed95b7d8fa2b
Carl Nettelblad <carl.nettelblad@gmail.com>
2020-08-25 10:50:56 +0200

    Added migration LastAlert to Subscriptions

---
+++ Migrations/20200825084856_SubscriberLastAlert.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tuck.Migrations
+{
+    public partial class SubscriberLastAlert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "LastAlert",
+                table: "Subscriptions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAlert",
+                table: "Subscriptions");
+        }
+    }
+}
