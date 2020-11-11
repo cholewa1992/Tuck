70bfdc89880d00167eb2e733d5305dcc9f3f2229
Jacob  Benjamin Cholewa <Jacob@cholewa.dk>
2020-11-11 18:25:56 +0100

    Added ninja pop feature

---
+++ Migrations/20201111172650_NinjaPop.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tuck.Migrations
+{
+    public partial class NinjaPop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Ninja",
+                table: "Buffs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ninja",
+                table: "Buffs");
+        }
+    }
+}
